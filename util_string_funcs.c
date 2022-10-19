3ccb846c0525ac17d31c66b5e7417d3377682684
stanley <stanley@kali.kara>
2022-10-19 21:38:34 +0100

    bug fix (handle_integer handles INT_MIN)

--- util_string_funcs.c
+++ util_string_funcs.c
@@ -27,11 +27,10 @@ int handle_string(char *ptr)
 	{
 		_putchar('(');
 		_putchar('n');
-		_putchar('u');
-		_putchar('l');
+		_putchar('i');
 		_putchar('l');
 		_putchar(')');
-		return (6);
+		return (5);
 	}
 
 	for (i = 0; ptr[i] != '\0'; i++)
